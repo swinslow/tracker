4787850bba146c45734f8c063bd2dd85715eb698
Steve Winslow <swinslow@gmail.com>
2019-03-09 21:37:36 -0500

    Interim commit for trait tracking
    Signed-off-by: Steve Winslow <swinslow@gmail.com>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import './App.css';
 import DayRow from './dayRow.js';
 import { Table } from 'semantic-ui-react';
 
+const TRAITS = [
+  "blade",
+  "calm",
+  "chaos",
+  "death",
+  "eye",
+  "fascination",
+  "hunger",
+  "key",
+  "life",
+  "prime",
+]
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -19,26 +32,18 @@ class App extends Component {
           "blade": [8, 3]
         }
       },
-      dates: {
-        /*7: {
-          "calm": [4, 3, true]
-        },
-        8: {
-          "calm": [4, 2, false]
-        },
-        9: {
-          "blade": [8, 3, true],
-          "calm": [4, 1, false]
-        },
-        10: {
-          "blade": [8, 2, false],
-          "calm": [2, 3, false]
-        }*/
-      }
+      dates: {}
     }
   }
 
   componentDidMount() {
+    this.resetDatesWithManual()
+
+//    this.clearManualValue(9, "blade");
+    this.setManualValue(15, "blade", 14, 3);
+  }
+
+  resetDatesWithManual() {
     // first, create dates object from manual
     let dates = {};
 
@@ -58,11 +63,11 @@ class App extends Component {
       dates[d] = newDate;
     }
 
-    // and finally, set the state
+    // set the state with the copied dates
     this.setState({dates: dates});
 
-//    this.clearManualValue(9, "blade");
-//    this.setManualValue(15, "blade", 8, 3);
+    // and now update the subsequent date values
+    this.updateValues();
   }
 
   getDayRows() {
@@ -96,9 +101,8 @@ class App extends Component {
   }
 
   setManualValue(intDate, trait, newValue, newDecay) {
-    this.setValue(intDate, trait, newValue, newDecay, true);
-
-    // also update manual list
+    // update manual list, and when it's done,
+    // trigger recalculating dates from manual
     this.setState(prevState => ({
       manual: {
         ...prevState.manual,
@@ -107,13 +111,12 @@ class App extends Component {
           [trait]: [newValue, newDecay]
         }
       }
-    }));
+    }), this.resetDatesWithManual);
   }
 
   clearManualValue(intDate, trait) {
-    this.setValue(intDate, trait, 0, 0, false);
-
-    // also remove from manual list
+    // remove from manual list, and when it's done,
+    // trigger recalculating dates from manual
     let revisedDate = this.state.manual[intDate]
     delete revisedDate[trait];
 
@@ -122,11 +125,140 @@ class App extends Component {
         ...prevState.manual,
         [intDate]: revisedDate
       }
-    }));
+    }), this.resetDatesWithManual);
   }
 
-  updateValuesInDateRange() {
+  // // returns true if there are any traits with values
+  // // for the given intDate.
+  // hasAnyTraitValues(intDate) {
+  //   date
+  // }
 
+  // // returns the intDate of the last day, prior to
+  // // beforeDate, on which there was no influence for
+  // // any trait, or 0 intDate at minimum.
+  // findLastEmptyDay(beforeDate) {
+
+  // }
+
+  // updates a single trait for a single day within a local
+  // dates object (e.g., does not itself set state), based on
+  // the values on the prior day within that date object.
+  updateValueForTraitOnDate(datesObj, intDate, trait) {
+    // if the intDate is <= 0, bail, we can't go any earlier
+    if (intDate <= 0) {
+      return;
+    }
+
+    // if today was manually set, just go with that
+    let dToday = datesObj[intDate]
+    if (dToday) {
+      let traitToday = dToday[trait]
+      if (traitToday) {
+        // was this trait manually set today?
+        if (traitToday[2]) {
+          // it was, so keep its value and exit
+          return;
+        }
+      }
+    }
+
+    // get the date object for the day before
+    let dYest = datesObj[intDate - 1]
+    if (!dYest) {
+      // not found, record 0 here and bail
+      datesObj[intDate] = {
+        ...datesObj[intDate],
+        [trait]: [0, 0, false]
+      };
+      return;
+    }
+
+    // get the value for this trait from yesterday
+    let tVal = dYest[trait]
+    if (!tVal) {
+      // no value yesterday, record 0 here and bail
+      datesObj[intDate] = {
+        ...datesObj[intDate],
+        [trait]: [0, 0, false]
+      };
+      return;
+    }
+
+    // now, decide how to set today's trait values based
+    // on yesterday's value and decay
+    let newDecay = tVal[1] - 1;
+    if (newDecay > 0) {
+      // we haven't decayed, so use the same trait value
+      // and just decrement the decay
+      datesObj[intDate] = {
+        ...datesObj[intDate],
+        [trait]: [tVal[0], newDecay, false]
+      };
+    } else {
+      // we've decayed, so adjust the trait value and
+      // reset the decay to what it should be, each
+      // depending on the trait
+      let newValue;
+      if (trait === "fascination") {
+        newValue = tVal[0] - 1;
+        newDecay = 7;
+      } else {
+        newValue = Math.floor(tVal[0] / 2);
+      }
+      datesObj[intDate] = {
+        ...datesObj[intDate],
+        [trait]: [newValue, newDecay, false]
+      };
+    }
+
+  }
+
+  updateValues() {
+    // first, create a copy of the existing dates object
+    let dates = {};
+
+    // walk through and copy dates => trait vals
+    // also note the min and max date as we proceed
+    let minDate = 999999;
+    let maxDate = 0;
+    const intDates = Object.keys(this.state.dates);
+    for (let i = 0; i < intDates.length; i++) {
+      let d = intDates[i];
+      if (d < minDate) {
+        minDate = d;
+      }
+      if (d > maxDate) {
+        maxDate = d;
+      }
+      let newDate = {};
+      const traits = Object.keys(this.state.dates[d]);
+      for (let j = 0; j < traits.length; j++) {
+        let trait = traits[j];
+        let traitVals = this.state.dates[d][trait]
+        newDate[trait] = [traitVals[0], traitVals[1], traitVals[2]];
+      }
+
+      // newDate's traits are now filled in; add to new object
+      dates[d] = newDate;
+    }
+
+    // okay, now the local dates object is a copy, so we don't
+    // have to worry about waiting for setState calls to
+    // propagate before continuing to the next day.
+
+    // we should be able to walk through in order, starting with
+    // the earliest date and continuing until the requested
+    // end date.
+    for (let iter = minDate; iter <= this.state.endDate; iter++) {
+      for (let t = 0; t < TRAITS.length; t++) {
+        console.log("updating " + iter + ", " + TRAITS[t]);
+        this.updateValueForTraitOnDate(dates, iter, TRAITS[t]);
+      }
+    }
+
+    // finally, at the end, set the state!
+    this.setState({dates: dates});
   }
 
   render() {
