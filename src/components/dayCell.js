97edb69e9a36492abdd210178046ecc8a7897ef8
Steve Winslow <swinslow@gmail.com>
2019-03-10 23:21:36 -0700

    Refactor editable cells into separate file and extend
    Signed-off-by: Steve Winslow <swinslow@gmail.com>

--- src/components/dayCell.js
+++ src/components/dayCell.js
@@ -1,97 +1,23 @@
 import React, { Component } from 'react'
-import { connect } from "react-redux";
 import { Table } from 'semantic-ui-react';
 import ManualMarker from './manualMarker';
-import { setManual } from "../actions";
+import DayCellValue from './dayCellValue';
 
-function mapDispatchToProps(dispatch) {
-    return {
-        setManual: (intDate, trait, newValue, newDecay) => dispatch(setManual(intDate, trait, newValue, newDecay))
-    };
-}
-
-class ConnectedDayCell extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            editing: false,
-            editContents: ""
-        }
-
-        this.handleClick = this.handleClick.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    makeValue() {
-        if (this.state.editing === true) {
-            return (
-                <span>
-                    <form onSubmit={this.handleSubmit}>
-                        <input value={this.state.editContents}
-                               onChange={event => this.onInputChange(event.target.value)} />
-                    </form>
-                </span>
-            )
-        } else {
-            return (
-                <span>{this.props.traitVals[0]} {this.checkMarker()}</span>
-            )
-        }
-    }
-
+class DayCell extends Component {
     checkMarker() {
         if (this.props.traitVals[2] === true) {
             return <ManualMarker intDate={this.props.intDate} trait={this.props.trait}/>
         }
     }
 
-    handleClick(event) {
-        event.preventDefault()
-        if (this.state.editing === false) {
-            console.log("Clicked on "+ this.props.trait + ", date " + this.props.intDate)
-            this.setState({
-                editing: true,
-                editContents: this.props.traitVals[0].toString()
-            })
-        }
-    }
-
-    onInputChange(editContents) {
-        this.setState({editContents})
-    }
-
-    handleSubmit(event) {
-        event.preventDefault()
-        let newValue = Number.parseInt(this.state.editContents)
-        console.log("Submitting manual value: " + this.props.trait + ", value " + newValue + ", date " + this.props.intDate)
-
-        // figure out correct decay value
-        let newDecay;
-        if (newValue === 0) {
-            newDecay = 0;
-        } else if (this.props.trait === "fascination") {
-            newDecay = 7;
-        } else {
-            newDecay = 3;
-        }
-
-        this.props.setManual(this.props.intDate, this.props.trait, newValue, newDecay)
-
-        // and reset / stop editing
-        this.setState({
-            editing: false,
-            editContents: ""
-        })
-    }
-
     render() {
         return (
-            <Table.Cell selectable onClick={this.handleClick} textAlign="center">{this.makeValue()}</Table.Cell>
+            <Table.Cell selectable onClick={this.handleClick} textAlign="center">
+                <DayCellValue intDate={this.props.intDate} trait={this.props.trait} traitVals={this.props.traitVals} />&nbsp;
+                {this.checkMarker()}
+            </Table.Cell>
         );
     }
 }
-
-const DayCell = connect(null, mapDispatchToProps) (ConnectedDayCell);
 
 export default DayCell;
---
+++ src/components/dayCellValue.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react'
+import { connect } from "react-redux";
+import { Button, Form, Input } from 'semantic-ui-react';
+import { setManual } from "../actions";
+
+function mapDispatchToProps(dispatch) {
+    return {
+        setManual: (intDate, trait, newValue, newDecay) => dispatch(setManual(intDate, trait, newValue, newDecay))
+    };
+}
+
+class ConnectedDayCellValue extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            editing: false,
+            editContents: ""
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
+    }
+
+    handleClick(event) {
+        event.preventDefault()
+        if (this.state.editing === false) {
+            console.log("Clicked on "+ this.props.trait + ", date " + this.props.intDate)
+            this.setState({
+                editing: true,
+                editContents: this.props.traitVals[0].toString()
+            })
+        }
+    }
+
+    handleCancel(event) {
+        event.preventDefault()
+        this.setState({
+            editing: false,
+            editContents: ""
+        })
+    }
+
+    onInputChange(editContents) {
+        this.setState({editContents})
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        let newValue = Number.parseInt(this.state.editContents)
+        if (Number.isNaN(newValue) || newValue < 0) {
+            newValue = 0;
+        }
+
+        console.log("Submitting manual value: " + this.props.trait + ", value " + newValue + ", date " + this.props.intDate)
+
+        // figure out correct decay value
+        let newDecay;
+        if (newValue === 0) {
+            newDecay = 0;
+        } else if (this.props.trait === "fascination") {
+            newDecay = 7;
+        } else {
+            newDecay = 3;
+        }
+
+        this.props.setManual(this.props.intDate, this.props.trait, newValue, newDecay)
+
+        // and reset / stop editing
+        this.setState({
+            editing: false,
+            editContents: ""
+        })
+    }
+
+    render() {
+        if (this.state.editing === true) {
+            return (
+                <span>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Input value={this.state.editContents}
+                               onChange={event => this.onInputChange(event.target.value)} />
+                        <Button size="mini" type="submit" content="Submit" />
+                        <Button size="mini" onClick={this.handleCancel}>X</Button>
+                    </Form>
+                </span>
+            )
+        } else {
+            return (
+                <span onClick={this.handleClick}>{this.props.traitVals[0]}</span>
+            )
+        }
+    }
+}
+
+const DayCellValue = connect(null, mapDispatchToProps) (ConnectedDayCellValue);
+
+export default DayCellValue;
